b6c593b40bdda402028709219975f0a862758737
Jay Krishna Reddy <majji.jaya_krishna_reddy@nokia.com>
2021-05-26 20:12:05 +0530

    Add project files.

---
+++ Logs_App_Insights/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logs_App_Insights.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger; // Adding the ILogger service
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger) // Injecting the Service
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var iteration = 1;
+            _logger.LogDebug($"Debug {iteration}");
+            _logger.LogInformation($"Information {iteration}");
+            _logger.LogWarning($"Warning {iteration}");
+            _logger.LogError($"Error {iteration}");
+            _logger.LogCritical($"Critical {iteration}");
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+            }
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
